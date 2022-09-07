2d82969a9e51d1b56ae58393d8a64625fd314f6f
natalia sizikova <mogilnaya2012@mail.ru>
2022-09-07 11:01:50 +0700

    решение 64 задачи

---
+++ task64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в
+//промежутке от M до N по убыванию.
+using static System.Console;
+using System.Linq;
+Clear();
+WriteLine("Введите число M");
+int numM = int.Parse(ReadLine()!);
+WriteLine("Введите число N");
+int numN = int.Parse(ReadLine()!);
+WriteLine(Stringofnumbers(numM, numN));
+
+string Stringofnumbers(int number1, int number2)
+{
+    if (number1 == number2) return $"{number1}";
+    else if (number1 > number2) return $"{number1} {Stringofnumbers(number2, number1 - 1)}";
+    else return $"{number2} {Stringofnumbers(number1, number2 - 1)}";
+
+}
